e17d6e65fb7375d6413f47c6288df323b90351ef
Lucas Sena <79860759+LucSena@users.noreply.github.com>
2024-05-23 21:44:53 -0400

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,13 +2,6 @@ import { PlusSquare, DotsThreeVertical, FolderSimplePlus, ArrowSquareLeft, Arrow
 
 export function Main() {
     return (
-        // {/* VAI TER 4 DIVS/ABAS, 
-        // 1* - UMA VAI SER A DE "NOTAS", ELA VAI OCUPAR 2/5 DO WIDTH DA TELA E O HEIGHT VAI ATÉ O FINAL E DENTRO DELA VAI TER VARIOS CARDS DAS ANOTAÇÕES
-        // 2* - ELA VAI OCUPAR O 3/5 DO WIDTH DA TELA E VAI E DE HEIGHT VAI FICAR EM 50% DA TELA, VAI SER A ABA DE CHECKLIST, AONDE VAI TER OS CARDS DA CHECKLIST
-        // 3* - VAI FICAR EM BAIXO DA ABA DE CHECKLIST, VAI OCUPAR METADE DO 3/5 DO WIDTH DA TELA E VAI OCUPAR METADE DO HEIGHT DA TELA, VAI SER A ABA DE CALENDARIO
-        // 4* - VAI FICAR AO LADO DA ABA DE CALENDARIO, OCUPANDO O RESTANTE DO CHECKLIST, VAI SER UMA ABA ESCRITA "DRAFT PAD" E VAI TER UM BOX ESCRITO Comece a escrever, para a pessoa digitar
-        // */}
-
         <div className="w-full h-full p-4 flex flex-col md:flex-row bg-slate-100">
             <div className="w-full md:w-2/5 h-full bg-white border border-gray-200 rounded-lg mb-4 md:mb-0">
                 <div className="flex justify-between">
@@ -160,7 +153,7 @@ export function Main() {
                         </p>
 
                         <div className="p-4 px-6 flex justify-between mt-auto">
-                            <div className="flex items-center border border-purple-700 bg-blue-200 text-purple-700 rounded p-1 px-2">
+                            <div className="flex items-center border border-purple-700 bg-purple-200 text-purple-700 rounded p-1 px-2">
                                 <span className="text-sm">Pessoal</span>
                             </div>
 
